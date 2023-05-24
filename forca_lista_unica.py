d1311a2beec975754149206a54ea63a6dfd71838
Vitória Freire <116108257+Vitoria-Freire@users.noreply.github.com>
2023-05-24 09:58:07 -0300

    Update forca_lista_unica.py
    Substituição das letras corretas foi efetuado com sucesso!

--- forca_lista_unica.py
+++ forca_lista_unica.py
@@ -1,5 +1,3 @@
-#aqui fica o boneco da forca
-#interface_nao_grafica
 print(" "*10,"JOGO DA FORCA"," "*10)
 print("\nOlá, seja bem vindo!")
 print("\nATENÇÃO!\n\nA cada rodada aparecerão duas dicas!\nVocê terá 5 chances de acertar a palavra!\n\nBoa Sorte!\n")
@@ -12,62 +10,57 @@ sorteio = random.choice(palavras)
 tentativas = 0
 chances = 5
 letras_usuario = []
+escolhas = []
 
 print(f"A palavra tem {len(sorteio)} letra\n")
 tracos = ("_") * len(sorteio)
 print(tracos, end=" \n\n")
 
-for j in range(0, len(sorteio)):
+for i in range(0, len(sorteio)):
     letras_usuario.append("_")
 
-while tentativas < chances:
-    if sorteio == "smartphone":
+if sorteio == "smartphone":
         print("1º DICA: Tecnologia\n2º DICA: Usamos no dia a dia")
-    if sorteio == "fogueira":
+if sorteio == "fogueira":
         print("1º DICA: Aquece\n2º DICA: Lenha")
-    if sorteio == "Silvio Santos":
+if sorteio == "Silvio Santos":
         print("1º DICA: Televisão\n2º DICA: aviãzinho")
-    if sorteio == "Queen":
+if sorteio == "Queen":
         print("1º DICA: Banda famosa\n2ºDICA: Champions League")
-    if sorteio == "manteiga":
+if sorteio == "manteiga":
         print("1º DICA: Derrete\n2º DICA: Amarelo")
-    if sorteio == "rede":
+if sorteio == "rede":
         print("1º DICA: Descanso\n2º DICA: Balança")
-    if sorteio == "piloto":
+if sorteio == "piloto":
         print("1º DICA: Cabine de avião\n2º DICA: Professor usa")
-    if sorteio == "Maria Leopoldina":
+if sorteio == "Maria Leopoldina":
         print("1º DICA: Imperatriz \n 2º DICA: Áustria")
-    if sorteio == "fronteira":
+if sorteio == "fronteira":
         print("1º DICA: Divisão\n2º DICA: Estar em dois lugares ao mesmo tempo")
 
+while tentativas < chances:
     letra = input("\nDigite uma letra: ")
 
-    while letra in letras_usuario: 
+    while letra in escolhas: 
         print("Não pode usar a mesma letra duas vezes!\n")
         letra = input("Digite uma letra: ")
     
-    letras_usuario.append(letra)
+    escolhas.append(letra)
     #substituindo as letras certas na palavra desconhecida
     for i in range(0, len(sorteio)):
         if letra == sorteio[i]:
             letras_usuario[i] = letra
+            print(letras_usuario)
 
     if letra in sorteio:
         print("\nVocê acertou uma letra!")
-        '''acertos = " "
-        for certo in sorteio: 
-            if certo in letras_usuario: #rever essa parte novamente
-                acertos += certo
-            else:
-                acertos += "_" '''
     else:
         print("\nNão foi dessa vez. Tente novamente!")
         tentativas += 1
-    print(f"Você tem {tentativas} tentativas e {chances-tentativas} chances")
-    #print(f"{acertos}\n")
+        print(f"Você tem {tentativas} erros e {chances-tentativas} chances")
     
-    if letra in letras_usuario:
-        print(f"Você já escolheu essas letras: {letras_usuario}\n")
+    if letra in escolhas:
+        print(f"Você já escolheu essas letras: {escolhas}\n")
     
 #fim do jogo
 if tentativas == chances:
